c6f1f8fde2fb7f69987b5bcbc883797b1f100b84
Raamis Hussain <raamishussain94@gmail.com>
2025-08-08 14:02:44 -0700

    updating gradio UI

--- app/app.py
+++ app/app.py
@@ -2,12 +2,11 @@ import gradio as gr
 import logging
 
 from app.config import VOLUME_NAME
-from app.inference import run_inference
+from app.inference import load_model, run_inference
 from modal import (
     App,
     asgi_app,
     Image,
-    mount,
     Volume,
 )
 
@@ -16,29 +15,44 @@ from modal import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Set up app
-app = App("Lung Cancer Detector")
-model_volume = Volume.persisted(VOLUME_NAME)
-
-image = Image.from_dockerfile(
-    "Dockerfile",
-    context_mount=mount(".", "/app").mount(model_volume, "/model"),
-)
+image = Image.from_dockerfile("Dockerfile")
+model_volume = Volume.from_name(VOLUME_NAME)
+app = App("lung_cancer_detector", image=image)
 
 
-@app.function(image=image, mounts=[model_volume])
-@asgi_app
+@app.function(image=image, volumes={"/model": model_volume})
+@asgi_app()
 def web():
     """Gradio front end UI"""
 
     def predict(img):
-        return run_inference(img)
+        model = load_model()
+        return run_inference(img, model)
 
-    iface = gr.Interface(
-        fn=predict,
-        inputs=gr.Image(type="pil"),
-        outputs=gr.Image(type="pil"),
-        title="Lung Cancer Detector",
-        description="Upload a chest PET/CT scan to detect tumors",
-    )
-    return iface.app
+    with gr.Blocks(title="Lung Cancer Detector", fill_width=True) as ui:
+
+        gr.Markdown(
+            """
+            # Lung Cancer Detector
+
+            Upload a chest PET/CT scan to detect tumors
+            """
+        )
+
+        with gr.Row():
+            with gr.Column():
+                input_img = gr.Image(type="pil", label="Upload Scan")
+                submit = gr.Button("Analyze")
+
+            with gr.Column():
+                output_img = gr.Image(
+                    type="pil", label="Scan with Detected Tumors"
+                )
+
+        submit.click(
+            fn=predict,
+            inputs=input_img,
+            outputs=output_img,
+        )
+
+    return ui
